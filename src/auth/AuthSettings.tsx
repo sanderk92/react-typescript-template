627ee83ff9557d838af2158a5d74c1a3976fd867
Sander <sanderkrabbenborg@hotmail.com>
2023-11-22 00:21:06 +0100

    Fix redirect url

--- src/auth/AuthSettings.tsx
+++ src/auth/AuthSettings.tsx
@@ -2,7 +2,7 @@ export const host = window.location.protocol + "//" + window.location.host
 
 export const authSettings = {
     authority: "https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_6wOiibFgH",
-    redirect_uri: host + "/redirect",
+    redirect_uri: host + "/redirect/",
     post_logout_redirect_uri: host + "/logout",
     response_type: "code",
     client_id: "7ogq7f8re2t778fs2b5sjf7avt",
